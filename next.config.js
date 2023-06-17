df1e862a9444f0d2198e371fb3544944df73ddb3
정성준 <seongjun.chung@supercat.co.kr>
2023-06-17 17:25:40 +0900

    🎉프로젝트 초기 환경 세팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: (() => {
+    let compilerConfig = {
+      // styledComponents 활성화
+      styledComponents: true,
+    };
 
-module.exports = nextConfig
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 프로덕션 환경에서는 리액트 테스팅 라이브러리에서 사용하는 data-testid 속성을 제거한다.
+        reactRemoveProperties: {
+          properties: ['^data-testid$'],
+        },
+      };
+    }
+
+    return compilerConfig;
+  })(),
+};
+
+module.exports = nextConfig;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,45 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { createGlobalStyle } from 'styled-components';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum--scale=5"
+        />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+const GlobalStyle = createGlobalStyle`
+html, body, textarea {
+  padding: 0;
+  mergin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans';
+}
+
+* {
+    box-sizing: border-box;
+}
+
+a {
+  cursor: pointer;
+  text-decoration: none;
+  transition: .25s;
+    color: #000;
+}
+
+ol, ul {
+  list-style: none;
+}
+`;
