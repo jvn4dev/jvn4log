6e1b4e28eb4fbbf85a6b30ccaf1c97fca990da40
정성준 <seongjun.chung@supercat.co.kr>
2023-06-18 22:52:38 +0900

    💄 favicon 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum--scale=5"
         />
+        <link rel="icon" href="/favicon.ico" />
         <title>jvn4blog</title>
         <meta property="og:locale" content="ko_KR" />
         <meta property="og:type" content="website" />
