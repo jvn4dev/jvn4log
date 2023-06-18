3d8b4f192f676a49b3c8ca45bf5fc2e151e6e4d4
정성준 <seongjun.chung@supercat.co.kr>
2023-06-18 22:46:01 +0900

    💄 헤더 로고 적용 및 메뉴바 구현

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,16 +1,74 @@
+import WavingHandIcon from '@mui/icons-material/WavingHand';
 import Link from 'next/link';
+import styled from 'styled-components';
+import { theme } from '@/themes';
 
 export const Navbar = () => {
   return (
-    <header>
-      <h1>Website Title</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <NavContainer>
+      <NavWrapper>
+        <MainIconWrapper>
+          <Link href="/">
+            <WavingIcon />
+            <Label>Hi there!</Label>
+          </Link>
+        </MainIconWrapper>
+        <MenuWrapper>
+          <Link href="/">
+            <Menu>Blog</Menu>
+          </Link>
+          <Link href="/">
+            <Menu>About</Menu>
+          </Link>
+        </MenuWrapper>
+      </NavWrapper>
+    </NavContainer>
   );
 };
+
+const NavContainer = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding-top: 50px;
+`;
+
+const NavWrapper = styled.div`
+  width: 90%;
+  max-width: 1140px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 70px;
+`;
+
+const MainIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const WavingIcon = styled(WavingHandIcon)`
+  font-size: 3rem;
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 300px;
+`;
+
+const Menu = styled.div`
+  font-size: 1.5rem;
+  font-weight: 300;
+  color: ${theme.colors.textDark};
+`;
+
+const Label = styled.label`
+  font-size: 1rem;
+  font-weight: 300;
+  color: ${theme.colors.textDark};
+  margin-left: 10px;
+  cursor: pointer;
+`;
