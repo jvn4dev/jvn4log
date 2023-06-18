5609798e42cbd9f70ce086f810d2e3c0522c5cbb
정성준 <seongjun.chung@supercat.co.kr>
2023-06-18 13:04:03 +0900

    ✨ ISR로 노션 데이터베이스 데이터 가져오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,38 @@
-import { Client } from '@notionhq/client';
 import { NextPage } from 'next';
+import Notion from '@/lib/notion';
+import { NotionData } from '@/types/notion';
 
 type HomeProps = {
-  notionData: any;
+  notionData: NotionData[];
 };
 
 const Home: NextPage<HomeProps> = (props) => {
-  console.log(props);
+  const { notionData } = props;
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1>정성준 블로그</h1>
+      <ul>
+        {notionData.map((data) => (
+          <li key={data.id}>
+            <a href={`/posts/${data.id}`}>{data.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export const getStaticProps = async () => {
-  const databaseId = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID ?? '';
-  const notion = new Client({ auth: process.env.NEXT_PUBLIC_NOTION_API_KEY });
+  const notionInstance = new Notion();
 
-  const response = await notion.databases.query({
-    database_id: databaseId,
-  });
+  const response = await notionInstance.getAllPublished();
 
   return {
     props: {
       notionData: response,
     },
+    revalidate: 60 * 5, // 5분마다 재생성
   };
 };
 
